d426ed606367c6e8d360a2541800f3081ea8a979
contant30 <romain.contant@hotmail.fr>
2025-11-13 11:13:51 +0100

    new

---
+++ src/main/java/entites.java
@@ -0,0 +1,16 @@
+public class entites {
+
+    class AdressePostale {
+
+        int numeroRue;
+        String lebelleRue;
+        int codePostal;
+        String ville;
+    }
+static void main     
+
+
+
+
+
+}
