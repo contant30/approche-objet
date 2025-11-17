feeacc7361431b1d9cbe5a980c7388cc9dcfa6a0
contant30 <romain.contant@hotmail.fr>
2025-11-17 17:00:58 +0100

    TP  Combat

--- src/main/java/fr/diginamic/combat/entites/Personnage.java
+++ src/main/java/fr/diginamic/combat/entites/Personnage.java
@@ -16,7 +16,35 @@ public class Personnage {
         this.scorePersonnage =0;
     }
 
-    // Getters
+
+    // Methode
+
+    // pour retourner true si point de vie sup a 0 sinon false
+    public boolean enVie(){
+        return pointsSantePersonnage >0;
+    }
+
+    // Methode pour recevoir des dégat
+    // Math.max permet de ne pas déscendre en dessous de 0.
+    public void recevoirDegats(int degats){
+        pointsSantePersonnage= Math.max(0,pointsSantePersonnage-degats);
+    }
+
+    // Methode pour attaquer
+    public int attaque(Personnage cible){
+        int degats = this.forcePersonnage;
+        cible.recevoirDegats(degats);
+        return degats;
+    }
+
+
+
+
+
+
+
+
+    // Getters pour lire les infos
     public String getNomPerso(){
         return nomPerso;
     }
@@ -30,6 +58,22 @@ public class Personnage {
         return scorePersonnage;
     }
 
+    // Setters
+    // SET modification des points de vie
+    public void setPointsSantePersonnage(int sante){
+        this.pointsSantePersonnage= sante;
+    }
+
+    // SET modification des points
+    public void setScorePersonnage(int score){
+        this.scorePersonnage= score;
+    }
+
+
+    /*@Override
+    public String toString() {
+        return String.format("%s [force=%d, pv=%d, score=%d]", nomPerso, force, pointsDeSante, score);
+    }*/
 
 
 
