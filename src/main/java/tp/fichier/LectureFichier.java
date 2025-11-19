08a018463439a769d7d46e8bb3db8f8dbea6c3f4
contant30 <romain.contant@hotmail.fr>
2025-11-19 14:02:47 +0100

    TP  Exercie  LireFichierAvecInstanciation

--- src/main/java/tp/fichier/LectureFichier.java
+++ src/main/java/tp/fichier/LectureFichier.java
@@ -4,18 +4,40 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LectureFichier {
     static void main(String[] args) throws IOException {
 
-        Path pathFile = Paths.get("C:/Users/romai/Downloads/recensement.csv");
+        // récupère les infos du fichier
+        Path monFichier = Paths.get("C:/Users/romai/Downloads/recensement.csv");
 
-        List<String> line = Files.readAllLines(pathFile);
+        // list du fichier pathFile
+        List<String> line = Files.readAllLines(monFichier);
 
-        // parcourt de chaque ligne du tableau et affiche chaque ligne du tableau
-        for (String lines: line){
-            System.out.println(lines);
+        // création d un nouveau tableau
+        List<String> ville= new ArrayList<>();
+
+        // parcour chaque ligne de la liste line
+        for (String lines : line){
+        String[] tokens = lines.split(";");
+
+        // recherche des variables dans different index
+        String nom = tokens[6];
+        String departementStr = String.valueOf(tokens[2]);
+        String nomRegion = tokens[1];
+        String populationStr = String.valueOf(tokens[7]);
+
+        ville.add(nom);
+
+//        System.out.println(nom);
+//        System.out.println(nomRegion);
+//        System.out.println(populationStr);
+//        System.out.println(departementStr);
         }
+
+        System.out.println(ville);
     }
 }
---
+++ src/main/java/tp/fichier/Ville.java
@@ -0,0 +1,59 @@
+package tp.fichier;
+
+public class Ville {
+
+    private String nom;
+    private int departement;
+    private String nomRegion;
+    private int population;
+
+    public Ville(String nom, int departement, String nomRegion, int population) {
+        this.nom = nom;
+        this.departement = departement;
+        this.nomRegion = nomRegion;
+        this.population = population;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getDepartement() {
+        return departement;
+    }
+
+    public void setDepartement(int departement) {
+        this.departement = departement;
+    }
+
+    public String getNomRegion() {
+        return nomRegion;
+    }
+
+    public void setNomRegion(String nomRegion) {
+        this.nomRegion = nomRegion;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "Ville{" +
+                "nom='" + nom + '\'' +
+                ", departement=" + departement +
+                ", nomRegion='" + nomRegion + '\'' +
+                ", population=" + population +
+                '}';
+    }
+}
