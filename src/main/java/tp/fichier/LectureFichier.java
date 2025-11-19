4f89a2fcedf9eccac0cc8fcbacb446fc0f91727e
contant30 <romain.contant@hotmail.fr>
2025-11-19 15:17:16 +0100

    TP  Exercie  LireFichierAvecInstanciation

--- src/main/java/tp/fichier/LectureFichier.java
+++ src/main/java/tp/fichier/LectureFichier.java
@@ -15,38 +15,40 @@ public class LectureFichier {
         Path monFichier = Paths.get("C:/Users/romai/Downloads/recensement.csv");
         Path outputFile = Paths.get("C:/Users/romai/Downloads/villes_de_plus_de_25k.csv");
 
-        // lecture de toutes les lignes
+        // Création list
         List<String> line = Files.readAllLines(monFichier);
 
         // création de la liste d'objets Ville
         List<Ville> villes = new ArrayList<>();
 
-        // parcours des lignes (ignore l'entête avec i=1)
+        // création de la liste à écrire, avec l entête
+        List<String> ville25K = new ArrayList<>();
+        ville25K.add("Nom;Code département;Nom region;Population totale");
+
+
+        // parcours des lignes (ignore l entête avec i=1)
         for (int i = 1; i < line.size(); i++) {
             String[] tokens = line.get(i).split(";");
-
             String nom = tokens[6];
             String departement = tokens[2];
             String nomRegion = tokens[1];
-            int population = Integer.parseInt(tokens[7].replace(" ", ""));
+            int population = Integer.parseInt(tokens[7].replaceAll(" ", ""));
 
             Ville villeAjout = new Ville(nom, departement, nomRegion, population);
             villes.add(villeAjout);
         }
 
-        // création de la liste à écrire, avec l'entête
-        List<String> ville25K = new ArrayList<>();
-        ville25K.add("Nom;Code departement;Nom region;Population totale");
 
+
+        // Récupère les villes à plus de 25000
         for (Ville v : villes) {
             if (v.getPopulation() > 25000) {
-                ville25K.add(v.getNom() + ";" + v.getDepartement()
-                        + ";" + v.getNomRegion() + ";" + v.getPopulation());
+                ville25K.add(v.getNom() + ";" + v.getDepartement()+ ";" + v.getNomRegion() + ";" + v.getPopulation());
             }
         }
+
         // Écriture du fichier
         Files.write(outputFile, ville25K);
-
         System.out.println("Fichier créé avec " + (ville25K.size() - 1) + " villes de plus de 25 000 habitants.");
     }
 }
