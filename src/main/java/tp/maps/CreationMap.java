45b24176e5666e8b52fc514ed5d2cd5e1f590cf1
contant30 <romain.contant@hotmail.fr>
2025-11-21 11:16:31 +0100

    Exercie  MAP ET HASHMAP

---
+++ src/main/java/tp/maps/CreationMap.java
@@ -0,0 +1,31 @@
+package tp.maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CreationMap {
+    static void main(String[] args) {
+
+        // Création des employés
+        Employes emp1 = new Employes("Paul ",1750 );
+        Employes emp2 = new Employes("Hicham ",1825 );
+        Employes emp3 = new Employes("Yu ",2250 );
+        Employes emp4 = new Employes("Ingrid ",2015 );
+        Employes emp5 = new Employes("Chantal ",2418 );
+
+
+
+        // Création d'une Map, la cle est de valeur String
+        Map<String, Employes> mapUser = new HashMap<>();
+
+        //ajout des valeurs dans la map
+        mapUser.put("Paul",emp1);
+        mapUser.put("Hicham",emp2);
+        mapUser.put("Yu",emp3);
+        mapUser.put("Ingrid", emp4);
+        mapUser.put("Chantal",emp5);
+
+        System.out.println("La taille de la map est de : "+mapUser.size());
+
+    }
+}
---
+++ src/main/java/tp/maps/Employes.java
@@ -0,0 +1,28 @@
+package tp.maps;
+
+public class Employes {
+
+    String nom;
+    int salaire;
+
+    public Employes(String nom, int salaire) {
+        this.nom = nom;
+        this.salaire = salaire;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getSalaire() {
+        return salaire;
+    }
+
+    @Override
+    public String toString() {
+        return "Employes{" +
+                "nom='" + nom + '\'' +
+                ", salaire=" + salaire +
+                '}';
+    }
+}
