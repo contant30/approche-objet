b35a7086a853e0e59ae91909479b19496e8fa959
contant30 <romain.contant@hotmail.fr>
2025-11-21 09:38:41 +0100

    TP  EXERCICE RECENSEMENT POPULATION

--- src/main/java/fr/diginamic/recensement/ApplicationRecensement.java
+++ src/main/java/fr/diginamic/recensement/ApplicationRecensement.java
@@ -1,9 +1,15 @@
 package fr.diginamic.recensement;
 
+import fr.diginamic.recensement.test.TesteRecherche;
+
+import java.io.IOException;
+
 public class ApplicationRecensement {
-    static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException {
+        TesteRecherche.main(args);
 
+        // Permet de lancer la recherche
 
     }
 }
